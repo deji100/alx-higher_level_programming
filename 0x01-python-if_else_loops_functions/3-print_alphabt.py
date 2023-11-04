de208d58729eeb96b6084e97e6c79cf36ffbf82a
deji100 <adeyemideji9@gmail.com>
2023-11-04 12:22:34 -0700

    .

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for c in range(ord("A"), ord("Z") + 1):
     if chr(c).lower() is not "e" and chr(c).lower() is not "p":
-        print("{}".format(chr(c).lower()), end=")
+        print("{}".format(chr(c).lower()), end="")
